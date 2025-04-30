e2b31db24c18320d693696a8ebd881eef88f9118
yuswita-sri <yuswitasri@gmail.com>
2025-04-30 20:34:11 +0700

    input data

---
+++ input_data.py
@@ -0,0 +1,10 @@
+nama = input("Masukan nama : ")
+print(nama)
+
+# input data integer
+umur = int(input("Masukan umur : "))
+print("usia kamu adalah : ", umur, " tahun")
+
+# input boolean
+bool = bool(int(input("Masukan angka boolean : ")))
+print(bool)
